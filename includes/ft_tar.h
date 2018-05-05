3148ba05e27b9003670e835890e3aa0f9f107e06
Andrii STADNIK <astadnik@student.unit.ua>
2018-05-05 13:49:36 +0300

    Created dirs

--- includes/ft_tar.h
+++
@@ -1,5 +0,0 @@
-#ifndef FT_TAR_T
-# define FT_TAR_T
-# include "../libft/includes/libft.h"
-
-#endif
--- srcs/test.c
+++
@@ -1,7 +0,0 @@
-#include "ft_tar.h"
-
-int	main(void)
-{
-	ft_printf("{red}Hello, World!{eoc}\n");
-	return (0);
-}
